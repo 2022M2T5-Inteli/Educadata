4d81f35ed0162bde4987c8eba846617bce29d1ef
PriscilaFalcao <priscila.santos@sou.inteli.edu.br>
2022-05-25 16:23:11 -0300

    LocalHost

---
+++ Projeto/Atual/app.js
@@ -0,0 +1,10 @@
+const express = require('express'); 
+const app = express();
+
+const hostname = '127.0.0.1';
+const port = 3022;
+app.use(express.static("./Inicio/"));
+
+app.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
