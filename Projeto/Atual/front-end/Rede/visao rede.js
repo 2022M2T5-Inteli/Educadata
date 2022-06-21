5a3545d8cb294512878aeed220a0217e7e8a86ed
Priscila Falcão <priscila.santos@sou.inteli.edu.br>
2022-06-21 14:52:32 -0300

    Atualizações V5

---
+++ Projeto/Atual/front-end/Rede/visao rede.js
@@ -0,0 +1,12 @@
+//Funções de troca de tela
+
+function profileBtn(){
+  document.getElementById("profile").style.display = "none";
+  document.getElementById("registeredSchools").style.display = "block";
+}
+
+function registeredSchoolsBtn(){
+  document.getElementById("profile").style.display = "block";
+  document.getElementById("registeredSchools").style.display = "none";
+}
+
