f0add22611622eb1906a0066484a733a2212155e
Priscila Falcão <priscila.santos@sou.inteli.edu.br>
2022-06-22 13:37:36 -0300

    Alterações

--- Projeto/Atual/front-end/Rede/visaoRede.js
+++ Projeto/Atual/front-end/Rede/visaoRede.js
@@ -1,30 +1,6 @@
-var answerButtonBeingUsed = false
-var multiplier = 0
-var currentQuestion = 0;
-var lastQuestion = 0;
-var primaryColor = '#948d28'
-var black = '#151515'
-var gray = '#C4C4C4'
-var verifier = 0
-var allPoints = [
-  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
-];
-var teaching = 0;
-var people = 0;
-var questions = []
-var QuestaoMatriz = {
-  pergunta: "null",
-  eixo: 1,
-  peso: 1,
-  respostas: []
-}
-var quest = 0
+//FUNCTION TO CHANGE PAGE
 
-
-
-//Funções de troca de tela
-
+//Profile:
 function profileBtn(){
   const profileBtn = document.getElementById("profileBtn")
   const editProfileBtn = document.getElementById("editProfileBtn")
@@ -35,18 +11,7 @@ function profileBtn(){
   document.getElementById("editProfile").style.display = "none";
 }
 
-
-//Desativa o botao de resultados (HARD-CODE)
-function resultsDisable(){
-  const btn = document.getElementById("resultsBtn")
-  var survey = 1;
-  if(survey == 0){
-    btn.classList.add("disabled")
-  }else{
-    btn.classList.remove("disabled")
-  };
-}
-
+// Edit profile:
 function editProfileBtn(){
   const profileBtn = document.getElementById("profileBtn")
   const editProfileBtn = document.getElementById("editProfileBtn")
@@ -58,4 +23,16 @@ function editProfileBtn(){
 }
 
 
+
+//FUNCTION TO DISABLED THE RESULT BUTTON (HARD-CODE)
+
+function resultsDisable(){
+  const btn = document.getElementById("resultsBtn")
+  var survey = 1;
+  if(survey == 0){
+    btn.classList.add("disabled")
+  }else{
+    btn.classList.remove("disabled")
+  };
+}
 
