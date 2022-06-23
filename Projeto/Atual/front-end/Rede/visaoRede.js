f3c57e9240d2172ba02efea5b38257348830468f
BartonThomas <99219127+BartonThomas@users.noreply.github.com>
2022-06-23 18:43:57 -0300

    get escolas rede

--- Projeto/Atual/front-end/Rede/visaoRede.js
+++ Projeto/Atual/front-end/Rede/visaoRede.js
@@ -20,6 +20,33 @@ function editProfileBtn(){
 
   document.getElementById("profile").style.display = "none";
   document.getElementById("editProfile").style.display = "block";
+  getEscola()
+}
+
+function getEscola(){
+  var url = "http://127.0.0.1:3080/escola/users";
+  var resposta;
+  var xhttp = new XMLHttpRequest();
+  xhttp.open("GET", url, false);
+  xhttp.send();//A execução do script pára aqui até a requisição retornar do servidor
+  resposta = JSON.parse(xhttp.responseText);
+  var instituicao = "";
+  var endereco = "";
+  var nFuncAluno = "";
+  var nAluno = "";
+  for(var i=0; i < resposta.length; i++){
+    instituicao += resposta[i].idEscola;
+    instituicao += ". ";
+    instituicao += resposta[i].Instituicao;
+    instituicao += "<hr class='solid'>"
+    instituicao += "</br>";
+    nAluno += resposta[i].nAluno;
+    nAluno += "<hr class='solid'>"
+    nAluno += "</br>";
+  }
+  document.getElementById("schoolList").innerHTML += ("<center>" + instituicao + "</center>");
+  document.getElementById("nAlunoList").innerHTML += ("<center>" + nAluno + "</center>");
+  //console.log(xhttp.responseText);
 }
 
 
