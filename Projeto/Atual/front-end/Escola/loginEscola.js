89b1164eee6cd688541db0f071c6cb7334862883
MaluMaiaa <99206636+MaluMaiaa@users.noreply.github.com>
2022-06-23 23:18:38 -0300

    AJAX - login escola para js

--- Projeto/Atual/front-end/Escola/loginEscola.js
+++ Projeto/Atual/front-end/Escola/loginEscola.js
@@ -11,3 +11,37 @@ btnSignin.addEventListener("click", function () {
 btnSignup.addEventListener("click", function () {
     body.className = "sign-up-js";
 })
+
+//FUNCTION AJAX - INSERT
+function insertEscola(){
+
+    $.ajax({
+        url: "http://127.0.0.1:3080/endereco/userinsert",
+        type: 'POST',
+        async: false,
+        data: {
+            idCEP: $("#cepCadastro").val(),
+            Pais: $("#pais").val(),
+            Estado: $("#estado").val(),
+            Cidade: $("#cidade").val(),
+            Bairro: $("#bairro").val(),
+            Rua: $("#rua").val(),
+            Numero: $("#numero").val(),
+            Complemento: $("#complemento").val(),
+        }
+    })
+
+    $.ajax({
+        url: "http://127.0.0.1:3080/escola/userinsert",
+        type: 'POST',
+        async: false,
+        data: {
+            Instituicao: $("#nomeEscolaCadastro").val(),
+            nAluno: $("#nAlunoCadastro").val(),
+            nFuncionario: $("#nFuncionarioCadastro").val(),
+            idRede: $("#redeID").val(),
+            idCEP: $("#cepCadastro").val(),
+            Email: $("#emailInstitucionalCadastro").val(),
+        }
+    }) 
+};
